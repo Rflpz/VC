bd3764e5ffa615b7ecfa855f31867920f3de4f95
Rflpz <rflpzdev@gmail.com>
2017-03-02 23:16:46 -0600

    Project setup

---
+++ VC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VC
+//
+//  Created by Rafael Lopez on 3/2/17.
+//  Copyright © 2017 Rflpz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
